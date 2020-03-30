256460a316ebd595e5b90a8200c8c10ae12f15fd
Alex <60114079+Sashimee@users.noreply.github.com>
2020-03-30 11:40:10 +0200

    Update import-12BC.php

--- import/import-12BC.php
+++ import/import-12BC.php
@@ -10,6 +10,7 @@
 <body>
     <?php
     $response = file_get_contents('https://api.themoviedb.org/3/movie/2?api_key=330e20146b752354b54e717c2df62353');
+    $response = json_decode($response);
     echo $response;
     ?>
 </body>
