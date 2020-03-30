3838ea12f48f89507cca33e12226b859576d7869
Alex <60114079+Sashimee@users.noreply.github.com>
2020-03-30 11:45:02 +0200

    Update import-12BC.php

--- import/import-12BC.php
+++ import/import-12BC.php
@@ -9,9 +9,16 @@
 
 <body>
     <?php
-    $response = file_get_contents('https://api.themoviedb.org/3/movie/2?api_key=330e20146b752354b54e717c2df62353');
-    $response = json_decode($response);
-    echo $response;
+    // $response = file_get_contents('https://api.themoviedb.org/3/movie/2?api_key=330e20146b752354b54e717c2df62353');
+    // $response = json_decode($response);
+    // echo $response;
+    $url = 'https://api.themoviedb.org/3/movie/2?api_key=330e20146b752354b54e717c2df62353';
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_HTTPGET, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response_json = curl_exec($ch);
+    curl_close($ch);
+    $response = json_decode($response_json, true);
     ?>
 </body>
 
