c652c73d75b638996670b8509e996747f9ba6fcc
Esserterp <60601358+Esserterp@users.noreply.github.com>
2020-03-30 15:48:12 +0200

    Update userpage.php

--- userpage.php
+++ userpage.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $user_id = $_SESSION['user_id'];
-$user_check_query = "SELECT * FROM user WHERE user_id= . $user_id";
+$user_check_query = "SELECT * FROM user WHERE user_id= $user_id";
 var_dump($user_check_query);
 $result = mysqli_query($db, $user_check_query);
 var_dump($result);
