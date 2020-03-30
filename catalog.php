ca3da1daacf2d3aca1b97d570283a9ec1901e819
IliaMaz <60601328+IliaMaz@users.noreply.github.com>
2020-03-30 14:21:42 +0200

    Update catalog.php
    works now.

--- catalog.php
+++ catalog.php
@@ -18,11 +18,17 @@
             $query = 'SELECT * FROM movie';
             $res = mysqli_query($connect, $query);
             while ($row = mysqli_fetch_assoc($res)) {
+                $image = $row['poster_url'];
+                if (strlen($image) == 33)
+                    $image = 'https://dummyimage.com/500x750/000/fff';
+                else
+                    $image = $row['poster_url'];
+
                 echo '
                     <div class="col s3">
                         <div class="card">
                             <div class="card-image">
-                                <img src="' . (strlen($row['poster_url']) === 33) ? 'https://dummyimage.com/500x750/000/fff' : $row['poster_url'] . '">
+                                <img src="' . $image . '">
                                <span class="card-title">' . $row['title'] . '</span>
                             </div>        
                         </div>
