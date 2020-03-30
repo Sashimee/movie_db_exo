cf3260086bff43a24e2881255a8a7b001acb657d
Esserterp <60601358+Esserterp@users.noreply.github.com>
2020-03-30 16:25:21 +0200

    Update userpage.php

--- userpage.php
+++ userpage.php
@@ -25,19 +25,19 @@ var_dump($user);
         <?php include('errors.php'); ?>
         <div class="input-group">
             <label>Username</label>
-            <input type="text" name="username" value="<?php echo $username; ?>">
+            <input type="text" name="username" value="<?php echo $user['username']; ?>" placeholder="<?php echo $user['username']; ?>">
         </div>
         <div class="input-group">
             <label>Email</label>
-            <input type="email" name="email" value="<?php echo $email; ?>">
+            <input type="email" name="email" value="<?php echo $user['email']; ?>" placeholder="<?php echo $user['email']; ?>">
         </div>
         <div class="input-group">
             <label>Password</label>
-            <input type="password" name="password_1" value="<?php echo $email; ?>>
+            <input type="password" name="password_1" value="">
         </div>
         <div class=" input-group">
             <label>Confirm password</label>
-            <input type="password" name="password_2" value="<?php echo $email; ?>>
+            <input type="password" name="password_2" value="">
         </div>
         <div class=" input-group">
             <button type="submit" class="btn" name="reg_user">Update</button>
