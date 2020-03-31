460cffba21f81cc2a91c146b3a5b092ca4f82a2a
Pio <sl_piotr@o2.pl>
2020-03-31 12:42:19 +0200

    Nav version 9999

--- nav-bar.php
+++ nav-bar.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <link rel="stylesheet" href="style/style.css">
   <title>Nav bard</title>
 </head>
 
@@ -30,6 +31,7 @@
 
   <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <script src="scripts/catalogScript.js"></script>
 
 </body>
 
