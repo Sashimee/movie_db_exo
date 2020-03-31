aaa93ab87a612a93195e1a2e77adecefbf7ca3e1
Alex <60114079+Sashimee@users.noreply.github.com>
2020-03-31 09:19:42 +0200

    Create details.php
    first iteration

---
+++ details.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="style/style.css">
+</head>
+
+<body id="cat-bod">
+    <?php
+    // DB Info + Connection
+    require_once 'database.php';
+    $connect = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
+    if (isset($_GET['movie_id'])) {
+        # code... for detail
+    } else {
+        echo 'No Movie clicked ....';
+    }
+    ?>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="scripts/catalogScript.js"></script>
+</body>
+
+</html>
