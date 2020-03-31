e0253e9528146570bf11d69a1dc12d5ef0fcf1f6
Esserterp <60601358+Esserterp@users.noreply.github.com>
2020-03-31 14:43:51 +0200

    Update catalog.php

--- catalog.php
+++ catalog.php
@@ -12,6 +12,7 @@
 include_once('DataPlaylist.php');
 include_once('database.php');
 $db = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
+$user_id = $_SESSION['user_id'];
 session_start(); ?>
 
 <body id="cat-bod">
