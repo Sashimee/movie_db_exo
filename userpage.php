cbd4a5f7aa54e4a9f4fbb90105d63d0caf40877e
Esserterp <60601358+Esserterp@users.noreply.github.com>
2020-03-31 09:32:54 +0200

    usrpag

--- userpage.php
+++ userpage.php
@@ -46,11 +46,13 @@ $user = mysqli_fetch_assoc($result);
                 $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
                 $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
                 $password = md5($user['password']);
-                $update_query1 = "UPDATE user SET email = '$email', username = '$username', password = '$password' WHERE user_id = '$user_id'";
+                $newUserEmail = $_POST['email'];
+                $newUsername = $_POST['usrname'];
+                $update_query1 = "UPDATE user SET email = '$newUserEmail', username = '$newUsername', password = '$password' WHERE user_id = '$user_id'";
                 mysqli_query($db, $update_query1);
                 var_dump($update_query1);
             } else {
-                $update_query2 = "UPDATE user SET email = '$email', username = '$username' WHERE user_id = '$user_id'";
+                $update_query2 = "UPDATE user SET email = '$newUserEmail', username = '$newUsername' WHERE user_id = '$user_id'";
                 mysqli_query($db, $update_query2);
                 var_dump($update_query2);
             }
