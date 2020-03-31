64718daa0b02f9201b4218102ed4af0b43c936e6
Alex <60114079+Sashimee@users.noreply.github.com>
2020-03-31 15:39:55 +0200

    Update catalogScript.js

--- scripts/catalogScript.js
+++ scripts/catalogScript.js
@@ -1,13 +1,20 @@
 $(".col").mouseenter(function(e) {
-  const infoCard = $(this).find(".info");
-  infoCard.show();
+    const infoCard = $(this).find(".info");
+    infoCard.show();
 });
 $(".col").mouseleave(function(e) {
-  const infoCard = $(this).find(".info");
-  infoCard.hide();
+    const infoCard = $(this).find(".info");
+    infoCard.hide();
 });
 
-$("option").click(function(e) {
-  console.log("ree");
-  console.log(this.val());
-});
+// $("option").click(function(e) {
+//   console.log("ree");
+//   console.log(this.val());
+// });
+
+$("option").on("click", clickFunction)
+
+function clickFunction() {
+    console.log("ree");
+    console.log(this.val());
+}
