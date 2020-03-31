e54221390bdef07dfe15df3c6be53215236151d5
IliaMaz <60601328+IliaMaz@users.noreply.github.com>
2020-03-31 15:52:52 +0200

    ree
    ree

--- catalog.php
+++ catalog.php
@@ -106,6 +106,12 @@ include_once('DataPlaylist.php'); ?>
     </div>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script>
+        $(function() {
+            console.log('yo');
+        });
+    </script>
+
     <script src="scripts/catalogScript.js"></script>
 </body>
 
