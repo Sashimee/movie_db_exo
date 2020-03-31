83ad7fb1c6c957dd43f7207377a3b5d6cdfed652
IliaMaz <60601328+IliaMaz@users.noreply.github.com>
2020-03-31 14:42:59 +0200

    Update catalog.php

--- catalog.php
+++ catalog.php
@@ -6,9 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Da Cat</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="style/style.css">
 </head>
-<?php require_once 'nav-bar.php'; ?>
+<?php require_once 'nav-bar.php';
+require_once 'database.php'; ?>
 
 <body id="cat-bod">
     <div class="container">
@@ -16,10 +18,9 @@
             <a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>Release</a>
             <div class="input-field col s12">
                 <select>
-                    <option value="" disabled selected>Choose your option</option>
+                    <option value="0" disabled selected>Choose your option</option>
                     <?php
                     $i = 0;
-                    require_once 'database.php';
                     $connect = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
                     $catQuery = 'SELECT DISTINCT category FROM movie';
                     $catRes = mysqli_query($connect, $catQuery);
