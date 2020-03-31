f324e453234a93ec6fecf43ac9b0e65db6cb4bc1
Pio <sl_piotr@o2.pl>
2020-03-31 12:23:37 +0200

    Update nav-bar.php

--- nav-bar.php
+++ nav-bar.php
@@ -62,7 +62,7 @@
   <nav>
     <div class="nav-wrapper grey darken-4">
       <span class="brand-logo">Anything The Movies</span>
-      <a href="userpage.php" class="brand-logo center">Welcome <strong><?php echo $_SESSION['username']; ?></strong></a>
+      <a href="userpage.php" class="brand-logo center">Welcome </a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="userpage.php">User</a></li>
         <li><a href="catalog.php">Movies</a></li>
