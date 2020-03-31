c3e7a3e6cd9d39dcf3fdcaa9d7e253a2134cc0e2
IliaMaz <60601328+IliaMaz@users.noreply.github.com>
2020-03-31 13:45:32 +0200

    Update catalog.php
    NO

--- catalog.php
+++ catalog.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link rel="stylesheet" href="style/style.css">
 </head>
-<?php require_once 'nav-bar.php'; ?>
 
 <body id="cat-bod">
     <div class="container">
