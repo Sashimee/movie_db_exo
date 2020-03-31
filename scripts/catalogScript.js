c82b4e89d06b4202beff00e56ceb7c523ce7a086
IliaMaz <60601328+IliaMaz@users.noreply.github.com>
2020-03-31 15:38:03 +0200

    Update catalogScript.js

--- scripts/catalogScript.js
+++ scripts/catalogScript.js
@@ -6,7 +6,6 @@ $(".col").mouseleave(function(e) {
   const infoCard = $(this).find(".info");
   infoCard.hide();
 });
-
 $("option").click(function(e) {
   console.log("ree");
   console.log(this.val());
